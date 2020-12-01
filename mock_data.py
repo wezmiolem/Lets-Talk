5c646e2189c0e248eefe16cf3b6eb15463507cd9
wezmiolem <ch.maciek@gmail.com>
2020-12-01 15:20:39 +0100

    mock data added

---
+++ mock_data.py
@@ -0,0 +1,25 @@
+question_headers = ['Question ID', 'Time of a submission', 'number of views', 'number of votes', 'type of a question',
+                    'question message', 'question image']
+
+questions = [{'id': 0, 'submission_time': 'today', 'view_number': 10, 'vote_number': 5, 'type': 'general',
+             'message': 'to be or not to be?', 'image': 'None'},
+             {'id': 1, 'submission_time': 'yesterday', 'view_number': 8, 'vote_number': 5, 'type': 'personal',
+              'message': 'you talkin to me?', 'image': 'None'},
+             {'id': 2, 'submission_time': 'tomorrow', 'view_number': 7, 'vote_number': 5, 'type': 'unknown',
+              'message': 'wasssup?', 'image': 'None'}]
+
+answers = [{'id': 0, 'submission_time': 'now', 'vote_number': 10, 'question_id': 0, 'message': 'of course',
+            'image': 'None'},
+           {'id': 1, 'submission_time': 'now', 'vote_number': 2, 'question_id': 1, 'message': 'not rly. nope',
+            'image': 'None'},
+           {'id': 2, 'submission_time': 'now', 'vote_number': 3, 'question_id': 2, 'message': 'all good',
+            'image': 'None'},
+           {'id': 3, 'submission_time': 'now', 'vote_number': 5, 'question_id': 2, 'message': 'not much rly',
+            'image': 'None'},
+           {'id': 4, 'submission_time': 'now', 'vote_number': 1, 'question_id': 1, 'message': 'thats right',
+            'image': 'None'},
+           {'id': 5, 'submission_time': 'now', 'vote_number': 8, 'question_id': 0, 'message': 'yes',
+            'image': 'None'},
+           {'id': 6, 'submission_time': 'now', 'vote_number': 3, 'question_id': 0, 'message': 'for sure',
+            'image': 'None'}]
+
